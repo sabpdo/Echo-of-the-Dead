41cb491e4d1ffa5299f3da8e78698c29ea1e3cac
Lara07t <laram@mit.edu>
2025-11-20 14:22:45 -0500

    homepage

--- scripts/how_to_play.gd
+++ scripts/how_to_play.gd
@@ -11,6 +11,6 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_home_page_button_pressed() -> void:
+func _on_button_pressed() -> void:
 	GameSettings.play_click_sound()
 	get_tree().change_scene_to_file(MAIN_MENU)
