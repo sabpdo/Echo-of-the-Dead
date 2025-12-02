bd8973d6f56419a403af6819733959f6ac900aa0
kbrought <kent88849@gmail.com>
2025-12-02 06:23:59 -0500

    Feat: Add build settings

--- scripts/InfoScreen.gd
+++ scripts/InfoScreen.gd
@@ -9,3 +9,6 @@ func _on_home_page_button_pressed() -> void:
 	GameSettings.play_click_sound()
 	get_tree().change_scene_to_file(MAIN_MENU)
 
+func _on_credits_label_meta_clicked(meta: Variant) -> void:
+	OS.shell_open(str(meta))
+
